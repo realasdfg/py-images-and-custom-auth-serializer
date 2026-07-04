7260b498acd5129fe8d44710226d2c10cc367ed9
Roman <realasdfga@gmail.com>
2026-07-04 22:08:38 +0200

    Change upload image path

--- cinema/models.py
+++ cinema/models.py
@@ -42,7 +42,7 @@ class Actor(models.Model):
 def image_path(instance, filename):
     _, extension = os.path.splitext(filename)
     return os.path.join(
-        "uploads/images/",
+        "uploads/movies/",
         f"{slugify(instance.title)}-{uuid.uuid4()}{extension}"
     )
 
